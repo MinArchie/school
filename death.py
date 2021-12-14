f1bf12d1014e6594ba200cb451adff86ee85f93d
MinArchie <93392453+MinArchie@users.noreply.github.com>
2021-12-14 17:50:22 +0530

    try this oit

--- death.py
+++ death.py
@@ -1,106 +1,251 @@
-while True:
+# a text-based adventure game set in a hospital
 
-    inventory = []
-    answer = int(input("You wake up, groggy and unsure. It's dark, so you turn on the light. You realize you're in a hospital. You decide to get out and explore. \nYou're in a long corridor. \n1) Go Left \n2) Go Right \n(Chose 1 or 2) \n"))
+# values: (keys)-> direction, (value)-> room
+rooms = {
+    "Corridor": {"West": "Bathroom", "or East": "Cafeteria"},
+    "Bathroom": {"Explore": "Red or Blue", "or East": "Corridor"},
+    "Cafeteria": {"East": "Staircase", "or West": "Corridor"},
+    "Staircase": {"West": "Cafeteria", "or North": "Terrace", "or South": "Waiting_Area"},
+    "Terrace": {"Jump": "Death", "or South": "Staircase"},
+    "Waiting_Area": {"North": "Staircase", "or South": "Exit", "or East": "Morgue", "or West": "Operating_Room"},
+    "Operating_Room": {"East": "Waiting_Area"},
+    "Morgue": {"West": "Waiting_Area"}
+}
 
-    #1: bathroom
-    if answer == 1:
-        answer = int(input("You walk down the hallway until you reach the bathrooms. You head inside to wash your face. You hear knocking coming from the last stall. \nWhat do you do? \n1) Explore the knocking \n2) Head back \n"))
-        #1: stay
-        if answer == 1:
-            answer = int(input("You open the bathroom stall to find a girl standing, holding out two color papers; one red and one blue. \nWhich one do you choose? \n1) Red \n2) Blue \n"))
-            #red
-            if answer == 1:
-                print("The girl pulls out a knife and stabs you. Should have left the bathroom when you had the chance. Better luck next time!")
-                ans = input("Restart Game? (Yes/No)").lower()
-                if ans == "yes":
-                    print("RESTARTING...")
-                    continue
-                else:
-                    print ("Thanks for playing :)")
-                    break
-                #blue
-            elif answer == 2:
-                print("The girl smiles and strangles you. Should have left the bathroom when you had the chance. Better luck next time!")
-                ans = input("Restart Game? (Yes/No)").lower()
-                if ans == "yes":
-                    print("RESTARTING...")
-                    continue
-                else:
-                    print("Thanks for playing :)")
-                    break
-        #2:leave
-        else:
-            answer = int(input("\nYou're in a long corridor. \n2) Go Right \n"))
-            if answer == 2:
-                answer = int(input(
-                    "You walk down the hallway until you reach the cafeteria. It is connected to stairway. The cafeteria smells funky. You see that all the food here are just human organs...?! \nWhat do you do? \n1) Run to the stairway \n2) Eat the organs! \n"))
-                # 1: stairway
-                if answer == 1:
-                    answer = int(input("You're in the stairway. \nWhich way do you go? \n1) Up \n2) Down \n"))
-                    # 1: terrace
-                    if answer == 1:
-                        answer = int(input(
-                            "You're in the Terrace. The stars look great tonight. There's nothing much here. \nWhat do you do? \n1) Jump off the building \n2)Head back. \n"))
-                        if answer == 1:
-                            print(
-                                    "The stars looked pretty. You wished to join them after all that you've witnessed. You died.")
-                            ans = input("Restart Game? (Yes/No)").lower()
-                            if ans == "yes":
-                                print("RESTARTING...")
-                                continue
-                            else:
-                                print("Thanks for playing :)")
-                                break
-                        elif answer == 2:
-                            answer = int(input("You're in the stairway. \nWhich way do you go? \n2) Down \n"))
 
-                # 2: cannibalism
-                elif answer == 2:
-                    print(
-                        "You try to eat the organs, but quickly realize that they were infected. You died. Perhaps cannibalism isn't the option.")
-                    ans = input("Restart Game? (Yes/No)").lower()
-                    if ans == "yes":
-                        print("RESTARTING...")
-                        continue
-                    else:
-                        print("Thanks for playing :)")
-                        break
+# functions
 
-    #2: cafeteria
-    elif answer == 2:
-        answer = int(input(
-            "You walk down the hallway until you reach the cafeteria. It is connected to stairway. The cafeteria smells funky. You see that all the food here are just human organs...?! \nWhat do you do? \n1) Run to the stairway \n2) Eat the organs!"))
-        # 1: stairway
-        if answer == 1:
-            answer = int(input("You're in the stairway. \nWhich way do you go? \n1) Up \n2) Down \n"))
-            # 1: terrace
-            if answer == 1:
-                answer = int(input("You're in the Terrace. The stars look great tonight. There's nothing much here. \nWhat do you do? \n1) Jump off the building \n2)Head back. \n"))
-                if answer == 1:
-                    print("The stars looked pretty. You wished to join them after all that you've witnessed. You died.")
-                    ans = input("Restart Game? (Yes/No)").lower()
-                    if ans == "yes":
-                        print("RESTARTING...")
-                        continue
-                    else:
-                        print("Thanks for playing :)")
-                        break
-            #2: head back
 
-        #2: cannibalism
-        elif answer == 2:
-            print("You try to eat the organs, but quickly realize that they were infected. You died. Perhaps cannibalism isn't the option.")
-            ans = input("Restart Game? (Yes/No)").lower()
-            if ans == "yes":
-                print("RESTARTING...")
-                continue
-            else:
-                print("Thanks for playing :)")
-                break
+def corridor():
+    location = "Corridor"
+    direction = ""
 
-    elif answer == 2:
-        print("idk")
+    while direction != "exit":
+        print("You are in the ", location)
 
-    else:
-        print ("Idk what you're saying mate. Type 1 or 2")
+        possible_moves = rooms[location].keys()
+        print("possible moves: ", *possible_moves)
+
+        direction = input("\nMove which direction? \n> ").strip().lower()
+        print("You entered: ", direction)
+
+        if direction == "west":
+            print()
+            bathroom()
+        elif direction == "east":
+            print()
+            cafeteria()
+
+
+def bathroom():
+    location = "Bathroom"
+    direction = ""
+
+    print("You are in the ", location)
+
+    print("You head inside to wash your face.")
+    print("You hear knocking coming from the last stall.")
+    print("\nWhat do you do? Explore the knocking or Head East (go back to corridor) \n")
+
+    possible_moves = rooms[location].keys()
+    print("Possible moves: ", *possible_moves)
+
+    direction = input("Move which direction? \n> ").strip().lower()
+    print("You entered: ", direction)
+
+    if direction == "east":
+        print()
+        corridor()
+    if direction == "explore":
+        print()
+        print("You see a girl holding two papers; one Red, one Blue.")
+        answer = input("Which do you choose? \n> Red \n> Blue \n>> ").lower().strip()
+        if answer == "red":
+            print()
+            print("The girl pulls out a knife and stabs you.")
+            print("Looks Like Curiosity Killed the Cat. Should have retreated when you had the chance. Better luck next time!")
+            restart_seq()
+        elif answer == "blue":
+            print()
+            print("The girl smiles and strangles you.")
+            print("Looks Like Curiosity Killed the Cat. Should have retreated when you had the chance. Better luck next time!")
+            restart_seq()
+
+
+def cafeteria():
+    location = "Cafeteria"
+    direction = ""
+
+    print("You are in the ", location)
+
+    print("It seems to be connected to the stairway.")
+    print("Suddenly, you smell something weird, like rotten meat.")
+    print("You cautiously look at the food there, and discover that they all look like organs.")
+    print("Human organs...")
+    answer1 = input("What do you do? \n>Run to the staircase! \n>Grab the thing (Type Run or Grab)")
+
+
+    possible_moves = rooms[location].keys()
+    print("Possible moves: ", *possible_moves)
+
+    direction = input("Move which direction? \n> ").strip().lower()
+    print("You entered: ", direction)
+
+    if direction == "east":
+        print()
+        staircase()
+    elif direction == "west":
+        print()
+        corridor()
+
+
+def staircase():
+    location = "Staircase"
+    direction = ""
+
+    print("You are in the ", location)
+
+    possible_moves = rooms[location].keys()
+    print("Possible moves: ", *possible_moves)
+
+    direction = input("Move which direction? \n> ").strip().lower()
+    print("You entered: ", direction)
+
+    if direction == "north":
+        print()
+        terrace()
+    elif direction == "south":
+        print()
+        waiting_area()
+    elif direction == "west":
+        print()
+        cafeteria()
+
+
+def terrace():
+    location = "Terrace"
+    direction = ""
+
+    print("You are in the, ", location)
+
+    possible_moves = rooms[location].keys()
+    print("Possible moves: ", *possible_moves)
+
+    direction = input("Move which direction? \n> ").strip().lower()
+    print("You entered: ", direction)
+
+    if direction == "south":
+        print()
+        staircase()
+    elif direction == "jump":
+        print()
+        print("You died")
+        restart_seq()
+
+
+def waiting_area():
+    location = "Waiting_Area"
+    direction = ""
+
+    while direction != "exit":
+        print("You are in the ", location)
+
+        print("You feel sick to the stomach, the sight of human organs still plaguing your thoughts.")
+        print("Never the less, you carry on.")
+        print("")
+
+        possible_moves = rooms[location].keys()
+        print("possible moves: ", *possible_moves)
+
+        direction = input("Move which direction? \n> ").strip().lower()
+        print("You entered: ", direction)
+
+        if direction == "west":
+            print()
+            operation_room()
+        elif direction == "north":
+            print()
+            staircase()
+        elif direction == "east":
+            print()
+            morgue()
+        elif direction == "south":
+            print()
+            print("You are now at the exit, but you need a 4-letter word to get through.")
+            print("Time is running out, the monsters won't wait for you.")
+            password()
+
+
+def password():
+    word = "echo"
+    guess = input("Guess the 4-letter word:")
+    tries = 1
+
+    if guess != word:
+        print("Wrong answer. Try again if you have the nerve.")
+        tries += 1
+        guess = input("Guess the 4-letter word:")
+    if guess != word and tries == 2:
+        print("You failed to guess the word was it that hard?\n")
+        restart_seq()
+
+    print("Well done you guessed correctly! The word was ECHO.")
+    restart_seq()
+
+
+def operation_room():
+    location = "Operating_Room"
+    direction = ""
+
+    print("You are in the, ", location)
+
+    possible_moves = rooms[location].keys()
+    print("Possible moves: ", *possible_moves)
+
+    direction = input("Move which direction? \n> ").strip().lower()
+    print("You entered: ", direction)
+
+    if direction == "east":
+        print()
+        waiting_area()
+
+
+def morgue():
+    location = "Morgue"
+    direction = ""
+
+    print("You are in the, ", location)
+
+    possible_moves = rooms[location].keys()
+    print("Possible moves: ", *possible_moves)
+
+    direction = input("Move which direction? \n> ").strip().lower()
+    print("You entered: ", direction)
+
+    if direction == "west":
+        print()
+        waiting_area()
+
+
+def restart_seq():
+    answer1 = input("Restart Game? (Yes/No) \n> ").strip().lower()
+    if answer1 == "yes":
+        print(""*5)
+        print("RESTARTING...")
+        print("You wake up groggy and unsure. It's dark, so you turn on the light.")
+        print("You realize you're in a hospital. You decide to get out and explore.")
+        corridor()
+
+    elif answer1 == "no":
+        print("Thank you for playing :)")
+        quit()
+
+
+# call stuff
+
+print()
+print()
+print("You wake up groggy and unsure. It's dark, so you turn on the light.")
+print("You realize you're in a hospital. You decide to get out and explore.")
+print()
+corridor()
