4cf179288896fd15ae652e374b7be3d9b5310bc0
MinArchie <93392453+MinArchie@users.noreply.github.com>
2021-12-14 18:29:38 +0530

    Some extra stuff
    i's okay if you dont like this change. it's just purely for the aesthetics. basically, I imported the time library and used the time.sleep() function. time.sleep() tells the code to 'sleep' for however long you set it to. like time.sleep(2) -> the code will wait two seconds before displaying the next line of code. it's cool and all, but if you think it's annoying then I'll remove it

--- death.py
+++ death.py
@@ -1,11 +1,13 @@
 # a text-based adventure game set in a hospital
 
+import time
+
 # values: (keys)-> direction, (value)-> room
 rooms = {
     "Corridor": {"West": "Bathroom", "or East": "Cafeteria"},
     "Bathroom": {"Explore": "Red or Blue", "or East": "Corridor"},
     "Cafeteria": {"East": "Staircase", "or West": "Corridor"},
-    "Staircase": {"West": "Cafeteria", "or North": "Terrace", "or South": "Waiting_Area"},
+    "Staircase": {"Back to Cafeteria (Type 'Back')": "Cafeteria", "or Up": "Terrace", "or Down": "Waiting_Area"},
     "Terrace": {"Jump": "Death", "or South": "Staircase"},
     "Waiting_Area": {"North": "Staircase", "or South": "Exit", "or East": "Morgue", "or West": "Operating_Room"},
     "Operating_Room": {"East": "Waiting_Area"},
@@ -44,13 +46,19 @@ def bathroom():
     print("You are in the ", location)
 
     print("You head inside to wash your face.")
+    time.sleep(1)
     print("You hear knocking coming from the last stall.")
+    time.sleep(2)
+    print("*knock*")
+    time.sleep(1)
+    print("*knock*.... *knock*")
+    time.sleep(1)
     print("\nWhat do you do? Explore the knocking or Head East (go back to corridor) \n")
 
     possible_moves = rooms[location].keys()
     print("Possible moves: ", *possible_moves)
 
-    direction = input("Move which direction? \n> ").strip().lower()
+    direction = input("Decision? \n> ").strip().lower()
     print("You entered: ", direction)
 
     if direction == "east":
@@ -58,16 +66,19 @@ def bathroom():
         corridor()
     if direction == "explore":
         print()
-        print("You see a girl holding two papers; one Red, one Blue.")
+        print("You see a girl holding two papers; one Red, one Blue. Her eyes are completely black")
+        time.sleep(2)
         answer = input("Which do you choose? \n> Red \n> Blue \n>> ").lower().strip()
         if answer == "red":
+            time.sleep(1)
             print()
             print("The girl pulls out a knife and stabs you.")
             print("Looks Like Curiosity Killed the Cat. Should have retreated when you had the chance. Better luck next time!")
             restart_seq()
         elif answer == "blue":
+            time.sleep(1)
             print()
-            print("The girl smiles and strangles you.")
+            print("The girl gives you a creepy smiles and strangles you.")
             print("Looks Like Curiosity Killed the Cat. Should have retreated when you had the chance. Better luck next time!")
             restart_seq()
 
@@ -79,21 +90,38 @@ def cafeteria():
     print("You are in the ", location)
 
     print("It seems to be connected to the stairway.")
+    time.sleep(2)
     print("Suddenly, you smell something weird, like rotten meat.")
     print("You cautiously look at the food there, and discover that they all look like organs.")
+    time.sleep(2)
     print("Human organs...")
-    answer1 = input("What do you do? \n>Run to the staircase! \n>Grab the thing (Type Run or Grab)")
+    time.sleep(1)
+    answer1 = input("What do you do? \n>Run to the staircase! \n>Eat the organs! \n(Type Run or Eat) \n>> ").lower().strip()
+    if answer1 == "run":
+        print()
+        print("The sight of the 'food' still makes you uncomfortable")
+        print("Perhaps you could go up for some fresh air or go down.")
+        staircase()
+
+
+    elif answer1 == "eat":
+        print()
+        print("You try to eat the organs, but quickly realize that they were infected. You died.")
+        print("Perhaps cannibalism isn't the option.")
+        restart_seq()
 
 
     possible_moves = rooms[location].keys()
     print("Possible moves: ", *possible_moves)
 
-    direction = input("Move which direction? \n> ").strip().lower()
+    direction = input("Move which direction? \n>> ").strip().lower()
     print("You entered: ", direction)
 
     if direction == "east":
-        print()
+        print("The sight of the 'food' still makes you uncomfortable")
+        print("Perhaps you could go up for some fresh air or go down.")
         staircase()
+
     elif direction == "west":
         print()
         corridor()
@@ -108,16 +136,16 @@ def staircase():
     possible_moves = rooms[location].keys()
     print("Possible moves: ", *possible_moves)
 
-    direction = input("Move which direction? \n> ").strip().lower()
+    direction = input("Move which direction? \n>> ").strip().lower()
     print("You entered: ", direction)
 
-    if direction == "north":
+    if direction == "up":
         print()
         terrace()
-    elif direction == "south":
+    elif direction == "down":
         print()
         waiting_area()
-    elif direction == "west":
+    elif direction == "back":
         print()
         cafeteria()
 
@@ -131,7 +159,7 @@ def terrace():
     possible_moves = rooms[location].keys()
     print("Possible moves: ", *possible_moves)
 
-    direction = input("Move which direction? \n> ").strip().lower()
+    direction = input("Move which direction? \n>> ").strip().lower()
     print("You entered: ", direction)
 
     if direction == "south":
@@ -157,7 +185,7 @@ def waiting_area():
         possible_moves = rooms[location].keys()
         print("possible moves: ", *possible_moves)
 
-        direction = input("Move which direction? \n> ").strip().lower()
+        direction = input("Move which direction? \n>> ").strip().lower()
         print("You entered: ", direction)
 
         if direction == "west":
@@ -178,15 +206,15 @@ def waiting_area():
 
 def password():
     word = "echo"
-    guess = input("Guess the 4-letter word:")
+    guess = input("Guess the 4-letter word (You have only 2 tries): ")
     tries = 1
 
     if guess != word:
-        print("Wrong answer. Try again if you have the nerve.")
+        print("Wrong answer. Try again, and get it right if you wish to live.")
         tries += 1
-        guess = input("Guess the 4-letter word:")
+        guess = input("Guess the 4-letter word: ")
     if guess != word and tries == 2:
-        print("You failed to guess the word was it that hard?\n")
+        print("You failed to guess the word. You died.\n")
         restart_seq()
 
     print("Well done you guessed correctly! The word was ECHO.")
@@ -202,7 +230,7 @@ def operation_room():
     possible_moves = rooms[location].keys()
     print("Possible moves: ", *possible_moves)
 
-    direction = input("Move which direction? \n> ").strip().lower()
+    direction = input("Move which direction? \n>> ").strip().lower()
     print("You entered: ", direction)
 
     if direction == "east":
@@ -219,7 +247,7 @@ def morgue():
     possible_moves = rooms[location].keys()
     print("Possible moves: ", *possible_moves)
 
-    direction = input("Move which direction? \n> ").strip().lower()
+    direction = input("Move which direction? \n>> ").strip().lower()
     print("You entered: ", direction)
 
     if direction == "west":
@@ -228,10 +256,13 @@ def morgue():
 
 
 def restart_seq():
-    answer1 = input("Restart Game? (Yes/No) \n> ").strip().lower()
+    answer1 = input("Restart Game? (Yes/No) \n>> ").strip().lower()
     if answer1 == "yes":
         print(""*5)
         print("RESTARTING...")
+        time.sleep(2)
+        print()
+        print()
         print("You wake up groggy and unsure. It's dark, so you turn on the light.")
         print("You realize you're in a hospital. You decide to get out and explore.")
         corridor()
@@ -245,6 +276,26 @@ def restart_seq():
 
 print()
 print()
+print()
+print("*********************")
+print("*       TITLE       *")
+print("*********************")
+print()
+print()
+print()
+time.sleep(2)
+print("Welcome!")
+print("The rules of the game are simple. Find a way to escape.")
+time.sleep(2)
+print("GAME LOADING...")
+time.sleep(1)
+print("HINT: use lowercases only")
+time.sleep(1)
+print("LOADING...")
+print()
+print()
+print()
+time.sleep(3)
 print("You wake up groggy and unsure. It's dark, so you turn on the light.")
 print("You realize you're in a hospital. You decide to get out and explore.")
 print()
